a73cfb64b627127d5d431a3714548680d5b0ef14
Cesar Borges <cesarborgesfilho@gmail.com>
2025-12-24 17:53:38 -0300

    Add Spring Boot skeleton

---
+++ src/test/java/com/cborges/calculator_exercise/CalculatorExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cborges.calculator_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
